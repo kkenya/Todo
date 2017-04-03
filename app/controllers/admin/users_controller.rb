59af6b30b3469884a7576f051013a15718fa8537
Kenya Kawatsu <3982ne@gmail.com>
2017-04-03 18:53:50 +0900

    config_rb deleted

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,47 @@
+class Admin::UsersController < Admin::Base
+  def index
+    @users = User.order("name")
+  end
+
+  def search
+    @users = User.search(params[:q])
+    render :index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to [:admin, @user, :tasks], notice: "会員情報を登録しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to [:admin, @user, :tasks], notice: "会員情報を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    redirect_to :admin_users, notice: "会員情報を削除しました"
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :administrator)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330025851) do
+ActiveRecord::Schema.define(version: 20170403074417) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "user_id",                      null: false
@@ -23,10 +23,11 @@ ActiveRecord::Schema.define(version: 20170330025851) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "name",                            null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "hashed_password"
+    t.boolean  "administrator",   default: false
   end
 
 end
