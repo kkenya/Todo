171c9973a662fc4e4d86bbd99632ac53ba7698a1
Kenya Kawatsu <3982ne@gmail.com>
2017-04-26 15:10:17 +0900

    マイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -35,15 +35,6 @@ ActiveRecord::Schema.define(version: 20170416221051) do
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table "user_images", force: :cascade do |t|
-    t.integer  "user_id",      null: false
-    t.binary   "data"
-    t.string   "content_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["user_id"], name: "index_user_images_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name",                            null: false
     t.datetime "created_at",                      null: false
